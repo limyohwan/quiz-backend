39697e2b89be20d8c7bb003eadc8a9aacd51ec4f
Yohwan Lim <dyghks7102@naver.com>
2024-06-02 23:21:05 +0900

    fix: secret키 수정

--- myapi/settings.py
+++ myapi/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-i6)(-^u+_bbp+%s!3wzlcuai$*xd#$rwshe(vsc9w2ifm9#zo^')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-i6)(-^u+_bbp+%s!3wzlcuai$*xd#$rwshe(vsc9w2ifm9#zo^')
+SECRET_KEY = 'django-insecure-i6)(-^u+_bbp+%s!3wzlcuai$*xd#$rwshe(vsc9w2ifm9#zo^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
